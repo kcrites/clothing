f2b97cf270ae51eb8fa24f1fa91cb45bdda1ad67
Ken Crites <kenrcrites@gmail.com>
2020-04-01 13:52:43 +0200

    Refactored signin components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -45,11 +45,12 @@ class SignIn extends React.Component {
                         handleChange={this.handleChange}
                         label='password'
                         required />
-                    
-                    <CustomButton type="submit">Sign in</CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>
-                        {' '}Sign in with Google{' '}
+                    <div className='buttons'>
+                        <CustomButton type="submit">Sign in</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            {' '}Sign in with Google{' '}
                         </CustomButton>
+                    </div>
                 </form>
             </div>
         )
