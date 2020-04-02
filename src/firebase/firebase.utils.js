63a3112887667636774125d5aa8c456c20008e62
Ken Crites <kenrcrites@gmail.com>
2020-04-02 15:17:52 +0200

    Added firebase utils and signup

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,30 @@ const config = {
     measurementId: "G-W6L2JJWMEJ"
   };
 
+  export const createUserProfileDocument = async (userAuth, ...additionalData) => {
+    if(!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        //If snapshot doesn't exist, create user in DB with this data
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }catch(error) {
+            console.log('error creating user', error.message);
+        }
+    }
+    return userRef;
+  };
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
--- src/pages/sign-in-up/sign-in-up.component.jsx
+++ src/pages/sign-in-up/sign-in-up.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sign-in-up.styles.scss';
 
 const SignInPage = () => (
-    <div className='signin'>
+    <div className='sign-in-and-up'>
         <SignIn />
+        <SignUp />
     </div>
 )
 
