c1bddb4cc6971074e88416f04372be3d8bdd6052
Ken Crites <kenrcrites@gmail.com>
2020-10-12 11:13:58 +0200

    Moved directory and shop data in reducer

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+    <PersistGate persistor={persistor}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
